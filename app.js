585ec65c551c9c1178d8955ebd97b3ee1334fa2e
Jordan <jordan.donguy@gmail.com>
2025-02-08 17:56:05 +0100

    Change navbar and presentation design

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const header = document.getElementsByTagName('header')[0];
 const name = document.getElementById('name');
 const navbar = document.getElementById('navbar');
 const listMenu = document.getElementById('list-menu');
+const backdropFilter = document.getElementById('backdropFilter');
 
 const présentation = document.getElementById('présentation');
 
@@ -18,8 +19,12 @@ window.onload = function() {
     navbar.style.opacity = '1';
     listMenu.style.opacity = '1';
 
-    présentation.style.clipPath = 'polygon(0% -90%,90% 100%, 0% 100%)';
-    
+    if (window.innerWidth > 500) {
+        présentation.style.clipPath = 'polygon(0% -90%,90% 100%, 0% 100%)';
+    } else {
+        présentation.style.clipPath = 'polygon(0% -60%,100% 100%, 0% 100%)';
+    };
+
     hello.style.color = 'rgb(185, 205, 230)';
     myName.style.color = 'rgb(255, 255, 255)';
     me.style.color = 'rgb(190, 240, 200)';
@@ -30,25 +35,39 @@ const navLink = document.getElementsByClassName('nav-link');
 listMenu.onclick = function() {
    if (navbar.style.display === 'none') {
         navbar.style.display = 'flex';
+        backdropFilter.style.display = 'flex'
     } else {
-        navbar.style.display = 'none'
+        navbar.style.display = 'none';
+        backdropFilter.style.display = 'none'
     };
 };
 
 window.onresize = function() {
     if (window.innerWidth > 950) {
-         navbar.style.display = 'flex'
-    }  else {
-        navbar.style.display = 'none'
+        navbar.style.display = 'flex';
+        backdropFilter.style.display = 'flex';
+    } else {
+        navbar.style.display = 'none';
+        backdropFilter.style.display = 'none';
     };
 };
+
+window.onresize = function() {
+    if (window.innerWidth > 500) {
+        présentation.style.clipPath = 'polygon(0% -90%,90% 100%, 0% 100%)';
+    } else {
+        présentation.style.clipPath = 'polygon(0% -60%,100% 100%, 0% 100%)';
+    }
+}
 
 for (let i = 0; i < navLink.length; i++) {
     navLink[i].onclick = function() {
         if (window.innerWidth < 950) {
-            navbar.style.display = 'none'
+            navbar.style.display = 'none';
+            backdropFilter.style.display = 'none'
         } else {
-            navbar.style.display = 'flex'
+            navbar.style.display = 'flex';
+            backdropFilter.style.display = 'flex'
         };
     }
 };
