1703a20dabd8462c0f08ddc661cbf8672312a821
Jordan <jordan.donguy@gmail.com>
2025-06-02 23:45:56 +0200

    Design change

--- app.js
+++ app.js
@@ -1,58 +1,16 @@
-const header = document.getElementsByTagName('header')[0];
-
-const name = document.getElementById('name');
 const navbar = document.getElementById('navbar');
-const listMenu = document.getElementById('list-menu');
 
 const présentation = document.getElementById('présentation');
 const Hi = document.getElementById('Hi');
+const downArrow = document.getElementById('down-arrow')
+
+const aPropos = document.getElementById('a-propos');
 
 window.onload = function () {
-    name.style.opacity = '1';
     navbar.style.opacity = '1';
-    listMenu.style.opacity = '1';
 
-    Hi.style.width = '100%';
-    Hi.style.color = 'rgb(255, 255, 255)';
+    aPropos.style.opacity = '1'
+    downArrow.style.opacity= '1';
 
-    if (window.innerWidth > 950) {
-        Hi.style.fontSize = '3rem';
-    } else if (window.innerWidth > 500) {
-        Hi.style.fontSize = '2.5rem';
-    } else {
-        Hi.style.fontSize = '2rem';
-    };
+    Hi.style.fontSize = '3rem';
 }
-
-const navLink = document.getElementsByClassName('nav-link');
-
-listMenu.onclick = function () {
-    if (navbar.style.display === 'none') {
-        navbar.style.display = 'flex';
-    } else {
-        navbar.style.display = 'none';
-    };
-};
-
-window.onresize = function () {
-    if (window.innerWidth > 950) {
-        navbar.style.display = 'flex';
-        Hi.style.fontSize = '3rem';
-    } else if (window.innerWidth > 500) {
-        navbar.style.display = 'none';
-        Hi.style.fontSize = '2.5rem';
-    } else {
-        navbar.style.display = 'none';
-        Hi.style.fontSize = '2rem';
-    };
-};
-
-for (let i = 0; i < navLink.length; i++) {
-    navLink[i].onclick = function () {
-        if (window.innerWidth < 950) {
-            navbar.style.display = 'none';
-        } else {
-            navbar.style.display = 'flex';
-        };
-    }
-};
