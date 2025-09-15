4f5d55c1be462382bd92cfedcbfb78f1d7931a68
JordanDonguy <jordan.donguy@gmail.com>
2025-09-15 21:16:15 +0200

    Remove one project and update Swiper design
    - Remove mini-reddit project (archived)
    - Update Swiper to show 1 slide per view
    - Switch to coverflow effect for better presentation

--- src/app/[locale]/components/ProjectCard.tsx
+++ src/app/[locale]/components/ProjectCard.tsx
@@ -17,22 +17,22 @@ export default function ProjectCard({ title, img, img_mobile, description, tech_
 
     <article key={title} className='min-h-[50svh] flex flex-col xl:flex-row h-full justify-center items-center gap-4 pb-12 lg:gap-x-12 xl:mr-4 xl:my-0'>
       <h3 className='absolute hidden xl:block top-8 text-2xl lg:text-3xl 2xl:text-4xl text-center'>{title}</h3>
-      <img src={img} alt={`${title} desktop screenshot`} className='hidden xl:block object-contain h-2/3 my-auto' />
-      <img src={img_mobile} alt={`${title} mobile screenshot`} className='xl:hidden w-full h-full object-contain rounded-asym-project border border-zinc-700 md:border-0 md:outline outline-zinc-700' />
+      <img src={img} alt={`${title} desktop screenshot`} className='hidden lg:block object-contain h-2/3 my-auto' />
+      <img src={img_mobile} alt={`${title} mobile screenshot`} className='lg:hidden w-full h-full object-contain rounded-asym-project border border-zinc-700 md:border-0 md:outline outline-zinc-700' />
       <div className='lg:max-h-3/5 min-h-2/3 xl:w-1/3 my-auto text-white text-start mx-4 xl:mx-0'>
         <h3 className='xl:hidden text-2xl lg:text-3xl mb-4 lg:mb-6 text-center'>{title}</h3>
         <div className="grid grid-cols-2 w-full gap-3 md:gap-6 mb-6">
           <a
             href={live_url}
             target="_blank"
-            className="h-12 text-sm md:text-base flex items-center justify-center text-center border rounded-full bg-gradient-to-l from-blue-600 hover:cursor-pointer hover:scale-115 active:scale-90 duration-150"
+            className="h-12 text-sm md:text-base flex items-center justify-center text-center border rounded-full bg-gradient-to-l from-blue-600 hover:cursor-pointer hover:lg-scale-110 hover:scale-105 active:scale-90 duration-150"
           >
             {t("live-app")}
           </a>
           <a
             href={github_url}
             target="_blank"
-            className="h-12 text-center border rounded-full flex justify-center items-center hover:cursor-pointer hover:scale-115 hover:bg-[rgba(60,60,82,0.2)] active:scale-90 duration-150"
+            className="h-12 text-center border rounded-full flex justify-center items-center hover:cursor-pointer hover:lg-scale-110 hover:scale-105 hover:bg-[rgba(60,60,82,0.2)] active:scale-90 duration-150"
           >
             <img src="/img/logo/GitHub.svg" alt="GitHub logo" className="mr-2 mb-[1px] h-6 w-6 md:mb-0" />
             <p className="text-sm md:text-base pt-[1px] md:h-5 md:pt-0">GitHub</p>
--- src/app/[locale]/components/ProjectsSection.tsx
+++ src/app/[locale]/components/ProjectsSection.tsx
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-import { Pagination, Navigation } from 'swiper/modules';
+import { Pagination, Navigation, EffectCoverflow } from 'swiper/modules';
 import { useTranslations } from "next-intl";
 import { SlideFromTopOrBottom } from "./animations/SlideFromTopOrBottom";
 import ProjectCard from './ProjectCard';
@@ -12,24 +12,23 @@ import ProjectCard from './ProjectCard';
 export default function ProjectsSection() {
   const t = useTranslations("projects")
 
-  const tsBadge = {name: "TypeScript", url: "https://img.shields.io/badge/TypeScript-3178C6?logo=TypeScript&logoColor=white&style=flat"};
-  const reactBadge = {name: "React.js", url: "https://img.shields.io/badge/React-61DAFB?logo=React&logoColor=white&style=flat"};
-  const reactRouterBadge = {name: "React Router", url: "https://img.shields.io/badge/React Router-CA4245?logo=reactrouter&logoColor=white&style=flat"};
-  const nextJsBadge = {name: "Next.js", url: "https://img.shields.io/badge/Next.js-000000?logo=next.js&logoColor=white&style=flat"};
-  const reduxBadge = {name: "Redux", url: "https://img.shields.io/badge/Redux-764ABC?logo=redux&logoColor=white&style=flat"};
-  const tailwindBadge = {name: "Tailwind", url: "https://img.shields.io/badge/Tailwind CSS-06B6D4?logo=tailwindcss&logoColor=white&style=flat"};
-  const nodeBadge = {name: "Node.js", url: "https://img.shields.io/badge/Node.js-5FA04E?logo=nodedotjs&logoColor=white&style=flat"};
-  const expressBadge = {name: "Express.js", url: "https://img.shields.io/badge/Express.js-000000?logo=express&logoColor=white&style=flat"};
-  const postgresqlBadge = {name: "PostgreSQL", url: "https://img.shields.io/badge/PostgreSQL-4169E1?logo=postgresql&logoColor=white&style=flat"};
-  const vercelBadge = {name: "Vercel", url: "https://img.shields.io/badge/Vercel-000000?logo=vercel&logoColor=white&style=flat"};
-  const prismaBadge = {name: "Prisma", url: "https://img.shields.io/badge/Prisma-2D3748?logo=prisma&logoColor=white&style=flat"};
-  const supabaseBadge = {name: "Supabase", url: "https://img.shields.io/badge/Supabase-3FCF8E?logo=supabase&logoColor=white&style=flat"};
-  const neonBadge = {name: "Neon", url: "/img/badge/neon.webp"};
-  const northflankBadge = {name: "Northflank", url: "/img/badge/Northflank.webp"};
-  const cloudflareBadge = {name: "Cloudflare", url: "https://img.shields.io/badge/Cloudflare-F38020?logo=cloudflare&logoColor=white&style=flat"};
-  const geminiBadge = {name: "Gemini", url: "https://img.shields.io/badge/Google Gemini-8E75B2?logo=google gemini&logoColor=white&style=flat"};
-  const mistralBadge = {name: "Mistral", url: "https://img.shields.io/badge/Mistral AI-FA520F?logo=Mistral AI&logoColor=white&style=flat"};
-  const jestBadge = {name: "Jest", url: "https://img.shields.io/badge/Jest-C21325?logo=Jest&logoColor=white&style=flat"};
+  const tsBadge = { name: "TypeScript", url: "https://img.shields.io/badge/TypeScript-3178C6?logo=TypeScript&logoColor=white&style=flat" };
+  const reactBadge = { name: "React.js", url: "https://img.shields.io/badge/React-61DAFB?logo=React&logoColor=white&style=flat" };
+  const reactRouterBadge = { name: "React Router", url: "https://img.shields.io/badge/React Router-CA4245?logo=reactrouter&logoColor=white&style=flat" };
+  const nextJsBadge = { name: "Next.js", url: "https://img.shields.io/badge/Next.js-000000?logo=next.js&logoColor=white&style=flat" };
+  const tailwindBadge = { name: "Tailwind", url: "https://img.shields.io/badge/Tailwind CSS-06B6D4?logo=tailwindcss&logoColor=white&style=flat" };
+  const nodeBadge = { name: "Node.js", url: "https://img.shields.io/badge/Node.js-5FA04E?logo=nodedotjs&logoColor=white&style=flat" };
+  const expressBadge = { name: "Express.js", url: "https://img.shields.io/badge/Express.js-000000?logo=express&logoColor=white&style=flat" };
+  const postgresqlBadge = { name: "PostgreSQL", url: "https://img.shields.io/badge/PostgreSQL-4169E1?logo=postgresql&logoColor=white&style=flat" };
+  const vercelBadge = { name: "Vercel", url: "https://img.shields.io/badge/Vercel-000000?logo=vercel&logoColor=white&style=flat" };
+  const prismaBadge = { name: "Prisma", url: "https://img.shields.io/badge/Prisma-2D3748?logo=prisma&logoColor=white&style=flat" };
+  const supabaseBadge = { name: "Supabase", url: "https://img.shields.io/badge/Supabase-3FCF8E?logo=supabase&logoColor=white&style=flat" };
+  const neonBadge = { name: "Neon", url: "/img/badge/neon.webp" };
+  const northflankBadge = { name: "Northflank", url: "/img/badge/Northflank.webp" };
+  const cloudflareBadge = { name: "Cloudflare", url: "https://img.shields.io/badge/Cloudflare-F38020?logo=cloudflare&logoColor=white&style=flat" };
+  const geminiBadge = { name: "Gemini", url: "https://img.shields.io/badge/Google Gemini-8E75B2?logo=google gemini&logoColor=white&style=flat" };
+  const mistralBadge = { name: "Mistral", url: "https://img.shields.io/badge/Mistral AI-FA520F?logo=Mistral AI&logoColor=white&style=flat" };
+  const jestBadge = { name: "Jest", url: "https://img.shields.io/badge/Jest-C21325?logo=Jest&logoColor=white&style=flat" };
 
   const projectList = [
     {
@@ -59,36 +58,34 @@ export default function ProjectsSection() {
       live_url: "https://app.guitar-shop.store",
       github_url: "https://github.com/JordanDonguy/guitar-shop-frontend",
     },
-    {
-      title: "Mini Reddit",
-      img: "/img/projects/mini-reddit.webp",
-      img_mobile: "/img/projects/mini-reddit-mobile.webp",
-      description: t("mini-reddit"),
-      tech_stack: [reactBadge, reduxBadge, cloudflareBadge],
-      live_url: "https://mini-reddit.pages.dev",
-      github_url: "https://github.com/JordanDonguy/mini-reddit",
-    },
-  ]
+  ];
 
   return (
     <section id="projects" className="relative w-full min-h-screen xl:h-screen">
       <img src="/img/left.svg" loading="lazy" alt="" className="absolute z-[-1] top-0 left-0 scale-y-[-1] max-w-1/2 lg:max-w-1/3 aspect-auto" />
       <img src="/img/right.svg" loading="lazy" alt="" className="absolute z-[-1] bottom-0 right-0 scale-y-[-1] max-w-1/2 lg:max-w-1/3 aspect-auto" />
       <SlideFromTopOrBottom>
-        <div className="max-w-full mx-auto h-full text-center pt-16 xl:pb-24">
+        <div className="max-w-[90%] mx-auto h-full text-center pt-16 xl:pb-24">
           <h2 className="text-4xl font-bold mb-8">{t("title")}</h2>
           <Swiper
-            modules={[Pagination, Navigation]}
-            loop={projectList.length > 3}
-            pagination={projectList.length > 3 ? { clickable: true } : false}
-            navigation={projectList.length > 3}
-            spaceBetween={40}
-            slidesPerView={1.2}
+            modules={[Pagination, Navigation, EffectCoverflow]}
+            loop={projectList.length > 2}
+            effect={'coverflow'}
             centeredSlides={true}
-            className="!hidden md:!block w-full h-full "
+            slidesPerView={'auto'}
+            coverflowEffect={{
+              rotate: 100,
+              stretch: 0,
+              depth: 100,
+              modifier: 1,
+            }}
+            navigation={projectList.length > 2}
+            spaceBetween={100}
+            pagination={projectList.length > 2}
+            className="!hidden md:!block w-full h-full mySwiper"
           >
             {projectList.map(project =>
-              <SwiperSlide key={project.title} className="h-auto min-h-[850px] xl:min-h-fit bg-glass rounded-asym border border-zinc-700 xl:mb-40">
+              <SwiperSlide key={project.title} className="h-auto min-h-[900px] lg:min-h-[1000px] xl:min-h-fit bg-glass rounded-asym border-y border-zinc-700 xl:mb-40">
                 <ProjectCard
                   title={project.title}
                   img={project.img}
